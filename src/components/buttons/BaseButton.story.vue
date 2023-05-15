0cfa7e234bcdd712047b434501c52275580ee5dc
Christopher Debray <christopher.debray@club-employes.com>
2023-05-15 21:17:21 +0200

    ADD basic / functionnal setup of histoire

---
+++ src/components/buttons/BaseButton.story.vue
@@ -0,0 +1,9 @@
+<template>
+  <Story>
+    <BaseButton/>
+  </Story>
+</template>
+
+<script setup lang="ts">
+import BaseButton from './BaseButton.vue'
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 /// <reference types="histoire" />
 
 import { defineConfig } from 'vite'
+import { HstVue } from '@histoire/plugin-vue'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   histoire: {
-    // your Histoire configuration
+    plugins: [
+      HstVue(),
+    ],
   },
 })
